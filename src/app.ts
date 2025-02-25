679ec5affbab1e4f8d7b2cd8176685d457407378
[Mainak Banduri] <[mainak407@gmail.com]>
2025-02-25 23:24:43 +0530

    added cron for sleep

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import routes from './routes';
 const cors = require('cors');
+import cron from 'node-cron'
+import axios from 'axios';
 
 const app = express();
 app.use(cors())
@@ -19,3 +21,13 @@ app.use('/Blog',routes.BlogRoute );
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+// Schedule the cron job to run every 30 minutes
+cron.schedule('*/30 * * * *', async () => {
+    try {
+      const response = await axios.get('https://bloging-backend-q74o.onrender.com');
+      console.log(`✅ Success: ${response.status} - ${response.statusText}`);
+    } catch (error:any) {
+      console.error(`❌ Error: ${error.response ? error.response.status : error.message}`);
+    }
+  });
